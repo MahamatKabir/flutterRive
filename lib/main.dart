5e295087c81f6c3517ebdd6449da86eb9e69f07b
MahamatKabir <mahamatkabir1@gmail.com>
2024-02-04 22:46:14 +0300

    rive_project

---
+++ lib/main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:loginriver/screens/onboding/components/sign_in_form.dart';
+import 'package:loginriver/screens/onboding/onboding_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'The Flutter Way',
+      theme: ThemeData(
+        scaffoldBackgroundColor: const Color(0xFFEEF1F8),
+        primarySwatch: Colors.blue,
+        fontFamily: "Intel",
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(foregroundColor: Colors.white),
+        ),
+        inputDecorationTheme: const InputDecorationTheme(
+          filled: true,
+          fillColor: Colors.white,
+          errorStyle: TextStyle(height: 0),
+          border: defaultInputBorder,
+          enabledBorder: defaultInputBorder,
+          focusedBorder: defaultInputBorder,
+          errorBorder: defaultInputBorder,
+        ),
+      ),
+      home: const SignInForm(),
+    );
+  }
+}
+
+const defaultInputBorder = OutlineInputBorder(
+  borderRadius: BorderRadius.all(Radius.circular(16)),
+  borderSide: BorderSide(
+    color: Color.fromARGB(255, 7, 42, 150),
+    width: 1,
+  ),
+);
---
+++ lib/screens/onboding/components/sign_in_dialog.dart
@@ -0,0 +1,180 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+import 'sign_in_form.dart';
+
+void showCustomDialog(BuildContext context, {required ValueChanged onValue}) {
+  showGeneralDialog(
+    context: context,
+    barrierLabel: "Barrier",
+    barrierDismissible: true,
+    barrierColor: Colors.black.withOpacity(0.5),
+    transitionDuration: const Duration(milliseconds: 400),
+    pageBuilder: (_, __, ___) {
+      return Center(
+        child: Container(
+          height: 650,
+          margin: const EdgeInsets.symmetric(horizontal: 16),
+          padding: const EdgeInsets.symmetric(vertical: 32, horizontal: 24),
+          decoration: BoxDecoration(
+            color: Colors.white.withOpacity(0.95),
+            borderRadius: BorderRadius.circular(0),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                offset: const Offset(0, 30),
+                blurRadius: 60,
+              ),
+              const BoxShadow(
+                color: Colors.black45,
+                offset: Offset(0, 30),
+                blurRadius: 60,
+              ),
+            ],
+          ),
+          child: Scaffold(
+            // backgroundColor: Colors.transparent,
+            body: Stack(
+              clipBehavior: Clip.none,
+              children: [
+                SingleChildScrollView(
+                  child: Column(
+                    children: [
+                      const Text(
+                        "Sign in",
+                        style: TextStyle(
+                          fontSize: 34,
+                          fontFamily: "Poppins",
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      const Padding(
+                        padding: EdgeInsets.symmetric(vertical: 16),
+                        child: Text(
+                          "Access to 240+ hours of content. Learn design and code, by building real apps with Flutter and Swift.",
+                          textAlign: TextAlign.center,
+                        ),
+                      ),
+                      const SignInForm(),
+                      const Row(
+                        children: [
+                          Expanded(
+                            child: Divider(),
+                          ),
+                          Padding(
+                            padding: EdgeInsets.symmetric(horizontal: 16),
+                            child: Text(
+                              "OR",
+                              style: TextStyle(
+                                color: Colors.black26,
+                                fontWeight: FontWeight.w500,
+                              ),
+                            ),
+                          ),
+                          Expanded(child: Divider()),
+                        ],
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 8, bottom: 24),
+                        child: ElevatedButton.icon(
+                          onPressed: () {
+                            // singIn(context);
+                          },
+                          style: ElevatedButton.styleFrom(
+                            backgroundColor: const Color(0xFFF77D8E),
+                            minimumSize: const Size(double.infinity, 56),
+                            shape: const RoundedRectangleBorder(
+                              borderRadius: BorderRadius.only(
+                                topLeft: Radius.circular(10),
+                                topRight: Radius.circular(25),
+                                bottomRight: Radius.circular(25),
+                                bottomLeft: Radius.circular(25),
+                              ),
+                            ),
+                          ),
+                          icon: const Icon(
+                            CupertinoIcons.arrow_right,
+                            color: Color(0xFFFE0037),
+                          ),
+                          label: const Text("Sign up"),
+                        ),
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          IconButton(
+                            onPressed: () {},
+                            padding: EdgeInsets.zero,
+                            icon: SvgPicture.asset(
+                              "assets/icons/email_box.svg",
+                              height: 64,
+                              width: 64,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            padding: EdgeInsets.zero,
+                            icon: SvgPicture.asset(
+                              "assets/icons/apple_box.svg",
+                              height: 64,
+                              width: 64,
+                            ),
+                          ),
+                          IconButton(
+                            onPressed: () {},
+                            padding: EdgeInsets.zero,
+                            icon: SvgPicture.asset(
+                              "assets/icons/google_box.svg",
+                              height: 64,
+                              width: 64,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                const Positioned(
+                  left: 0,
+                  right: 0,
+                  bottom: -40,
+                  child: CircleAvatar(
+                    radius: 16,
+                    backgroundColor: Color.fromARGB(255, 0, 0, 0),
+                    child: Icon(
+                      Icons.close,
+                      size: 20,
+                      color: Colors.white70,
+                    ),
+                  ),
+                )
+              ],
+            ),
+          ),
+        ),
+      );
+    },
+    transitionBuilder: (_, anim, __, child) {
+      Tween<Offset> tween;
+      // if (anim.status == AnimationStatus.reverse) {
+      //   tween = Tween(begin: const Offset(0, 1), end: Offset.zero);
+      // } else {
+      //   tween = Tween(begin: const Offset(0, -1), end: Offset.zero);
+      // }
+
+      tween = Tween(begin: const Offset(0, -1), end: Offset.zero);
+
+      return SlideTransition(
+        position: tween.animate(
+          CurvedAnimation(parent: anim, curve: Curves.easeInOut),
+        ),
+        // child: FadeTransition(
+        //   opacity: anim,
+        //   child: child,
+        // ),
+        child: child,
+      );
+    },
+  ).then(onValue);
+}
